d4291ab808c92e3335933120cb49b062ec7de064
mohamedallam13 <mohamedallam.tu@gmail.com>
2021-11-28 20:27:59 +0100

    Add tests

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,81 @@
+const Manager = require('../lib/Manager');
+
+describe('Manager Class', () => {
+    // Arrange
+
+    const name = "Test";
+    const email = "test@test.com";
+    const param = { officeNumber: "1" }
+    const managerObj = new Manager(name, email, param);
+
+    // Name test
+    it("should return the name correctly", () => {
+
+        // Act
+        const returnedName = managerObj.getName();
+
+        // Assert
+        expect(returnedName).toEqual(name);
+    });
+
+    // Id test
+    it("should return an Id", () => {
+
+        // Act
+        const returnedId = managerObj.getId();
+
+        // Assert
+        expect(returnedId).toBeDefined()
+    });
+
+    // Email test
+    it("should return the correct Email", () => {
+
+        // Act
+        const returnedEmail = managerObj.getEmail();
+
+        // Assert
+        expect(returnedEmail).toEqual(email);
+    });
+
+    // Role test
+    it("should return 'Manager'", () => {
+
+        // Act
+        const returnedRole = managerObj.getRole();
+
+        // Assert
+        expect(returnedRole).toEqual("Manager");
+    });
+
+    //Proper Label Test
+    it("should return 'Office Number'", () => {
+
+        // Act
+        const returnedProperLabel = managerObj.extraInfoProper;
+
+        // Assert
+        expect(returnedProperLabel).toEqual("Office Number");
+    });
+
+    // School test
+    it("should return the school name", () => {
+
+        // Act
+        const returnedOfficeNumber = managerObj.getOfficeNumber();
+
+        // Assert
+        expect(returnedOfficeNumber).toEqual(param.officeNumber);
+    });
+
+    // School HTML string test
+    it("should return school name", () => {
+
+        // Act
+        const returnedOfficeNumber = managerObj.getExtraInfo();
+
+        // Assert
+        expect(returnedOfficeNumber).toEqual(param.officeNumber);
+    });
+
+});
